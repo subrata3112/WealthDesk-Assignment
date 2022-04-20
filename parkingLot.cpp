47dd0eb06b36d15e12d226f4a5145e8a0f5cda42
Subrata Kumar Biswas <43948261+subrata3112@users.noreply.github.com>
2022-04-20 21:10:35 +0530

    Created all required classes and objects.

--- parkingLot.cpp
+++ parkingLot.cpp
@@ -6,7 +6,10 @@ class ParkingLot{
 
     int parkingLotId;
     Location location;
+    int carCapacity;
+    int bikeCapacity;
     vector<Parking> parkingSlots;
+    RateChart ratechart;
 };
 
 class Location{
@@ -16,13 +19,18 @@ class Location{
 };
 
 class Parking{
-
     int parkingId;
     ParkingStatus status;
-    vector<Vehicle> ParkedVehicles;
+    vector<Vehicle> parkedVehicles;
 };
 
 enum ParkingStatus{Alloted,Free,UnderMantainance,BikeOnly};
+
+class RateChart{
+    int perHourBike;
+    int perHourCar;
+    int perHourHeavy;
+};
 
 class Vehicle{
     string vehicleId;
@@ -30,11 +38,44 @@ class Vehicle{
     vector<ParkingInfo> parkingHistory;
 };
 
-enum VehicleType{Bike,Car};
+enum VehicleType{Bike,Car,Heavy};
 
 class ParkingInfo{
 
     Vehicle vehicle;
     time_t startTime;
     time_t exitTime;
+};
+
+class Bill{
+    ParkingLot parkingLot;
+    Vehicle vehicle;
+    int getTotalAmount();
+};
+
+class User{
+    string firstName;
+    string lastName;
+    Account accountDetails;
+};
+
+class Account{
+
+    int uniqueId;
+    string password;
+};
+
+class Guard: public User{
+    
+    ParkingLot parkingLot;
+    vector<Parking> searchParkings(Vehicle);
+    void parkVehicle(Parking,Vehicle);
+    void freeParking(Parking);
+};
+
+class Admin: public User{
+
+    vector<ParkingInfo> getParkingHistory(Vehicle v);
+    void addParkingLot();
+    void addParking(ParkingLot);
 };
