580aec4d7f6c918c48d85058b59eb7514a5a7b82
Subrata Kumar Biswas <43948261+subrata3112@users.noreply.github.com>
2022-04-21 12:34:51 +0530

    Wrote methods within the classes.

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ Contents:-
 - Use-Case Diagram
 - LLD Design
 - HLD approches (beta)
+
+######################################################################################################
+
+Known Bugs/Missings:-
+
+- There is no class for Parking Slots. So app doesn't realises whether it freed a bike space or car space.
+- No setters/getters used due to lack of time.
+- For sake of simplicity, only hourly basis of rates are described in rateChart.
+- Admins can only access one parking lot at a time.
+- No errors/exception handling methods applied.
+- No authentication added. User class is not getting completely used.
+- User Info not getting saved. 
--- parkingLot.cpp
+++ parkingLot.cpp
@@ -4,65 +4,121 @@ using namespace std;
 
 class ParkingLot{
 
-    int parkingLotId;
-    int carCapacity;
-    int bikeCapacity;
+    public:
+        int parkingLotId;
+        int carCapacity;
+        int bikeCapacity;
+        bool sharedBikeParked;
     // vector<Parking> parkingSlots;
-    vector<Vehicle> vehiclesList;
-    RateChart ratechart;
+        vector<Vehicle> vehiclesList;
+        RateChart ratechart;
+
+        ParkingLot(int id,int car_capacity,int bike_capacity){
+            parkingLotId = id;
+            carCapacity = car_capacity;
+            bikeCapacity = bike_capacity;
+        }
 };
 
 class RateChart{
-    int perHourBike;
-    int perHourCar;
-    int perHourHeavy;
+    public:
+        int perHourBike;
+        int perHourCar;
+        int perHourHeavy;
 };
 
 class Vehicle{
-    string vehicleId;
-    VehicleType type;
-    vector<ParkingInfo> parkingHistory;
+    public:
+        string vehicleId;
+        VehicleType type;
+        ParkingInfo currentInfo;
+        vector<ParkingInfo> parkingHistory;
+        bool inCarParking;
 };
 
 enum VehicleType{Bike,Car,Heavy};
 
 class ParkingInfo{
-
-    Vehicle vehicle;
-    time_t startTime;
-    time_t exitTime;
-};
-
-class Bill{
-    ParkingLot parkingLot;
-    Vehicle vehicle;
-    int getTotalAmount();
+    public:
+        int parkingLotId;
+        tm *startTime;
+        tm *exitTime;
+        int amountPaid;
 };
 
 class User{
-    string firstName;
-    string lastName;
-    Account accountDetails;
+    public:
+        string firstName;
+        string lastName;
+        Account accountDetails;
 };
 
 class Account{
-
-    int uniqueId;
-    string password;
+    public:
+        int uniqueId;
+        string password;
 };
 
 class Guard: public User{
-    
-    ParkingLot parkingLot;
-    bool searchParkings(Vehicle v);
-    void parkVehicle(Vehicle v);
-    void freeParking(Vehicle v);
+    public:
+        ParkingLot parkingLot;
+        bool searchParkings(Vehicle v){
+            if(v.type == Bike){
+                if(parkingLot.bikeCapacity > 0) return true;
+                else if(parkingLot.carCapacity > 0) return true;
+                else return false;
+            }
+
+            else if(parkingLot.carCapacity == 0) return false;
+        };
+        void parkVehicle(Vehicle v){
+            if(v.type == Bike){
+                if(parkingLot.bikeCapacity > 0) parkingLot.bikeCapacity--;
+                else if(parkingLot.carCapacity > 0  && parkingLot.sharedBikeParked == false){
+                    parkingLot.carCapacity--;
+                    parkingLot.sharedBikeParked = true;
+                    v.inCarParking = true;
+                }
+                else {
+                    parkingLot.sharedBikeParked = false;
+                    v.inCarParking = true;
+                }
+            }
+            else parkingLot.carCapacity--;
+            parkingLot.vehiclesList.push_back(v);
+            time_t t_time = time(0);
+            tm *local_time = localtime(&t_time);
+            v.currentInfo.startTime = local_time;
+        };
+        void freeParking(Vehicle v){
+            time_t t_time = time(0);
+            tm *local_time = localtime(&t_time);
+            v.currentInfo.exitTime = local_time;
+            int hours = v.currentInfo.exitTime->tm_hour - v.currentInfo.startTime->tm_hour + 1;
+            if(v.type == Bike) v.currentInfo.amountPaid = hours*parkingLot.ratechart.perHourBike;
+            else if(v.type == Car) v.currentInfo.amountPaid = hours*parkingLot.ratechart.perHourCar;
+            else v.currentInfo.amountPaid = hours*parkingLot.ratechart.perHourHeavy;
+
+            v.parkingHistory.push_back(v.currentInfo);
+            if(v.type == Bike && !v.inCarParking) parkingLot.bikeCapacity++;
+            else if(v.type == Bike && v.inCarParking) parkingLot.carCapacity++;
+            else parkingLot.carCapacity++;
+        };
 };
 
 class Admin: public User{
-
-    vector<ParkingInfo> getParkingHistory(Vehicle v);
-    void addParkingLot();
-    void addParking(ParkingLot);
-    vector<ParkingLot> parkingLots;
+    public:
+        vector<ParkingLot> parkingLots;
+        void addParkingLot(int carCapacity,int bikeCapacity){
+            ParkingLot p1(123,carCapacity,bikeCapacity);
+            parkingLots.push_back(p1);
+        };
+        vector<ParkingInfo> getParkingHistory(string vehicle_id){
+            ParkingLot p = parkingLots[parkingLots.size() - 1];
+            Vehicle v;
+            for(auto i: p.vehiclesList){
+                if(i.vehicleId == vehicle_id) v = i;
+            }
+            return v.parkingHistory;
+        };
 };
